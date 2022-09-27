489290f4b06e277bc18bba6bd8a5ee52dcbf9598
Rifat <mdrifatahmed787@gmail.com>
2022-09-27 15:51:13 +0600

    cart added

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart }) => {
+    // console.log(cart)
+
+
+    let total = 0;
+    let shipping = 0;
+
+
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+
+
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = (total) + (shipping) + (tax);
+
+
+
+
     return (
-        <div>
+        <div className='cart'>
             <h4>Order summary</h4>
             <h2>Select Items: {cart.length}</h2>
+            <p>Total Price: ${total}</p>
+            <p>Total shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
